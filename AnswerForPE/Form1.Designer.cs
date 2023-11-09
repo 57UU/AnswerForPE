5d0ce092ad0eacdab4c2c3636e0a963ea606db39
57U <wuqiyou8@gmail.com>
2023-11-09 21:07:31 +0800

    添加项目文件。

---
+++ AnswerForPE/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AnswerForPE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1201, 675);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1226, 54);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label.Font = new System.Drawing.Font("微软雅黑", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label.Location = new System.Drawing.Point(3, 9);
+            this.label.Margin = new System.Windows.Forms.Padding(3, 9, 3, 0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(99, 38);
+            this.label.TabIndex = 0;
+            this.label.Text = "加载中";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(114, 9);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(9, 9, 3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1106, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 738);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1226, 38);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(470, 31);
+            this.toolStripStatusLabel1.Text = "使用方法：在输入框中输入题目中的关键字";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(34, 31);
+            this.toolStripStatusLabel2.Text = "--";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(167, 31);
+            this.toolStripStatusLabel3.Text = "Made By 57U";
+            this.toolStripStatusLabel3.Click += new System.EventHandler(this.toolStripStatusLabel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1226, 776);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "体育理论助手";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ AnswerForPE/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnswerForPE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //List<string> list ;
+        Dictionary<string, Entry> dict = new Dictionary<string, Entry>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+            //richTextBox1.Enabled = false;
+            toolStripStatusLabel2.Alignment = ToolStripItemAlignment.Right;
+            StreamReader file=null;
+            try
+            {
+                file = File.OpenText("data_enhance.txt");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error!can not find data_enhance.txt");
+            }
+            
+            var txt_raw = file.ReadToEnd();
+            var splited = txt_raw.Split(new string[] { "\r\n\r\n", }, StringSplitOptions.RemoveEmptyEntries);
+/*            var list =new List<string>();
+            foreach (var i in splited)
+            {
+                bool isAlreadyAdd = false;
+                var split_symbol = new string[] { "A\r\n", "B\r\n", "C\r\n", "D\r\n", "正确\r\n", "错误\r\n" };
+                foreach (var s in split_symbol)
+                {
+                    var slice = i.Split(new string[] { s }, StringSplitOptions.RemoveEmptyEntries);
+                    if (slice.Length > 1)
+                    {
+                        isAlreadyAdd = true;
+                        for (int count = 0; count < slice.Length - 1; count++)
+                        {
+                            list.Add(slice[count] + s);
+                        }
+                        list.Add(slice.Last());
+                        break;
+                    }
+                }
+                if (!isAlreadyAdd)
+                {
+                    list.Add(i);
+                }
+            }*/
+
+            foreach (var i in splited)
+            {
+                if (i == "\r\n")
+                {
+                    continue;
+                }
+                int index=i.IndexOf("\r\n");
+                string question = i.Substring(0, index);
+                string answer = i.Substring(index, i.Length-index);
+                if (dict.ContainsKey(question))
+                {
+                    if (dict[question].Prority < 1)
+                    {
+                        Entry entry1 = new Entry(question, answer);
+                        if (entry1.Prority > 0)
+                        {
+                            dict[question] = entry1;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Entry entry1 = new Entry(question, answer);
+                    dict[question] = entry1;
+                }
+            }
+       
+
+            label.Text = "关键字";
+            textBox1.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                richTextBox1.Text = "在输入框中输入题目关键字以查询";
+                return;
+            }
+            //GCSettings.LatencyMode = GCLatencyMode.Batch;
+            richTextBox1.Text = "";
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            foreach (var item in dict)
+            {
+                if (item.Key.Contains(textBox1.Text)||item.Value.Answer.Contains(textBox1.Text))
+                {
+                    sb.Append(item.Key);
+                    //sb.Append('\n');
+                    sb.Append(item.Value.Answer);
+                    sb.Append("\n\n");
+                    count++;
+                }
+            }
+            richTextBox1.Text += sb.ToString();
+            toolStripStatusLabel2.Text = $"共找到{count}个答案";
+            //GCSettings.LatencyMode = GCLatencyMode.Interactive;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel3_Click(object sender, EventArgs e)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;    //不使用shell启动
+            p.StartInfo.RedirectStandardInput = true;//喊cmd接受标准输入
+            p.StartInfo.RedirectStandardOutput = false;//不想听cmd讲话所以不要他输出
+            p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            p.StartInfo.CreateNoWindow = true;//不显示窗口
+            p.Start();
+
+            //向cmd窗口发送输入信息 后面的&exit告诉cmd运行好之后就退出
+            p.StandardInput.WriteLine("start " + "https://github.com/57UU" + "&exit");
+            //p.StandardInput.WriteLine("iexplore.exe " + url + "&exit");
+            p.StandardInput.AutoFlush = true;
+            //p.WaitForExit();//等待程序执行完退出进程
+            p.Close();
+        }
+    }
+    class Entry
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int Prority { get; private set; } = 0;
+        public Entry(string question, string answer)
+        {
+            Question = question;
+
+            int index = answer.IndexOf("标准答案");
+            if (index != -1)
+            {
+                int index2 = answer.IndexOf("你的答案");
+                if (index2 != -1)
+                {
+                    Answer = answer.Substring(0, index2);
+                    Answer += answer.Substring(index);
+                }
+                else
+                {
+                    Answer = answer;
+                }
+ 
+                Prority = 1;
+            }
+            else
+            {
+                Answer = answer;
+            }
+
+        }
+        public override int GetHashCode()
+        {
+            return Question.GetHashCode();
+        }
+    }
+}
